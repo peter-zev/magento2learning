0ae411b859aa0bbc05c27ecb70952713fb38baf8
Peteris Zevalds <peteris.zevalds@magebit.com>
2021-04-14 13:04:00 +0300

    first learning task complete

---
+++ public/app/code/Magebit/AttributeAllowed/ViewModel/AttributeAllowed.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peteriszevalds
+ * Date: 21.13.4
+ * Time: 12:52
+ */
+
+namespace Magebit\AttributeAllowed\ViewModel;
+
+
+class AttributeAllowed
+{
+
+}
---
+++ public/app/code/Magebit/StockQty/ViewModel/StockQty.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peteriszevalds
+ * Date: 21.13.4
+ * Time: 11:58
+ */
+
+namespace Magebit\StockQty\ViewModel;
+
+
+class StockQty
+{
+
+}
